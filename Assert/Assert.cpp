d248f8f6044bacd2c234e56554a9d3c16805c350
baguette <baguetteshimada@gmail.com>
2025-04-02 05:39:16 +0800

    Assert practise.

---
+++ Assert/Assert.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <cassert>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    assert(a > b);  // 5 > 10，为 false，触发断言失败
+    std::cout << "This will not be printed." << std::endl;
+    return 0;
+}
